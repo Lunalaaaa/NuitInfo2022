53a73de2e57877415431c7d7ebf57ee89816a5dc
Lunalaaaa <kicien.tania@gmail.com>
2022-12-01 22:13:08 +0100

    conf.php update

---
+++ site/Conf.php
@@ -0,0 +1,29 @@
+<?php
+
+class Conf {
+    static private array $databases = array(
+        'hostname' => 'https://phpmyadmin.alwaysdata.com/',
+        'database' => 'les-randoms_data',
+        'login' => '291341',
+        'password' => '123Random?'
+    );
+
+    static public function getLogin() : string {
+        // L'attribut statique $databases s'obtient avec la syntaxe static::$databases
+        // au lieu de $this->databases pour un attribut non statique
+        return static::$databases['login'];
+    }
+
+    static public function getHostName() : string {
+        return static::$databases['hostname'];
+    }
+
+    static public function getDatabase() : string {
+        return static::$databases['database'];
+    }
+
+    static public function getPassword() : string {
+        return static::$databases['password'];
+    }
+
+}
