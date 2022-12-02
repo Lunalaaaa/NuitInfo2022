d0972cd482cad1b185d9b3edc687aae5530e94c6
Lunalaaaa <kicien.tania@gmail.com>
2022-12-02 05:30:02 +0100

    vue question ok

--- src/View/detail.php
+++ src/View/detail.php
@@ -1,2 +1,12 @@
 <?php
-    echo "";
+
+use App\Site\Repository\UtilisateurRepository;
+
+    echo "<p>Question : ". $question.getIdQuestion() ."</p>";
+    echo "<p>Titre : ". $question.getTitre(). "</p>";
+    echo "<p>Description : " . $question.getDescription() . "</p>";
+    echo "<p>Question de " . UtilisateurRepository::select($question.getIdUtilisateur()) . "</p>";
+    echo "<p>Maladie concernée : " . $question.getNomMaladie() . "</p>";
+    echo "<p>Réponse : " . $question.getIdReponseA().getDescription() ."</p>";
+    echo "<p>Pseudo Utilisateur : " . $question.getPseudoUtilisateur() . "</p>";
+    echo "<p>Pseudo Utilisateur Reponse" . $question.getPseudoReponseA() . "</p>";
