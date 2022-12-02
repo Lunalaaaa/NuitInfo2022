c5c100733bb5622c434b8980a9d932c65306b05e
MCBE Craft <mcbe.craft0@gmail.com>
2022-12-02 07:32:08 +0100

    fix everything

--- secret.php
+++ secret.php
@@ -4,6 +4,4 @@ $loader = new App\Site\Lib\ClassLoader();
 $loader->addNamespace('App\Site', __DIR__ . '/src');
 $loader->register();
 
-
-if (!isset($_GET['p'])) $_GET['p'] = 'index';
-\App\Site\Controller\Controller::afficheVue('/Histoire/' . $_GET['p'] . '.html');
+\App\Site\Controller\Controller::afficheVue('secret.html', 'Bravo');
